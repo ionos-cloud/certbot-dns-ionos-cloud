540fe114973185e343dd0734825aed0c822863a5
zak905 <zakaria.amine88@gmail.com>
2024-03-18 16:37:15 +0100

    feat(certbot dns plugin): inital functionality and scaffolding [DNS-1270]

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+install_requires = [
+    "acme>=0.29.0",
+    "certbot>=0.34.0",
+    "setuptools",
+    "requests",
+    #"mock",
+    #"requests-mock",
+]
+
+
+setup(
+    name="certbot-dns-ionos",
+    version="0.1.0",
+    description="Certbot DNS Authenticator plugin for IONOS",
+    url="https://github.com/ionos-cloud/certbot-dns-ionos-plugin",
+    author="IONOS Cloud DNS team",
+    author_email="paas-dns@ionos.com",
+    license="Apache License 2.0",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    classifiers=[],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        "certbot.plugins": [
+            "dns-ionos = certbot_dns_ionos.ionos:Authenticator"
+        ]
+    },
+    test_suite="",
+)
